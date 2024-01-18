f9b2c4fb592c6d21393e3287aa18d4ffe31954c4
jasurbek22 <yusupovjasurbek75@gmail.com>
2024-01-18 01:16:53 -1100

    javascriptda ishladim

---
+++ script.js
@@ -0,0 +1,15 @@
+function askAge() {
+    var age = parseInt(prompt("Yoshingizni kiriting", "0dan 100gacha"));
+
+    if (isNaN(age) || age < 0 || age > 100) {
+        alert("Siz noto'g'ri kiritdingiz. Iltimos qaytadan kiriting.");
+        return askAge();
+    } else {
+        alert("Siz " + age + " yoshdasiz.");
+        document.write("<img src=./assets/maxresdefault.jpg>")
+        return age
+    }
+}
+
+var age = askAge();
+console.log("Sizning yoshingiz:" + age + " da")
